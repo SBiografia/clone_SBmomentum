1576175555a83b33a31a4df6e4f5efe8d34afa84
SBiografia <dltmdqh0208@naver.com>
2021-02-17 17:11:33 +0900

    unsplash API 이용해서 BG 이미지 변경

--- bg.js
+++ bg.js
@@ -4,7 +4,8 @@ const IMG_NUMBER = 5;
 
 function paintImage(imgNumber){
     const img = new Image();
-    img.src = `images/${imgNumber+1}.jpg`;
+    //img.src = `images/${imgNumber+1}.jpg`;
+    //img.src = 'https://source.unsplash.com/category/nature/1600x900';
     img.classList.add("bgImage");
     body.appendChild(img);
 }
@@ -15,7 +16,19 @@ function getRandom(){
 }
 
 function init(){
+    /* 기존에는 저장되어 있는 이미지 5장 중에서 랜덤으로 고르는거였으나.
     const randomNumber = getRandom();
     paintImage(randomNumber);
+    */
+   /* https://source.unsplash.com/featured/?{KEYWORD},{KEYWORD} 
+    ex) https://source.unsplash.com/1600x900/?nature,water */
+    //unsplash api 를 이용해서 랜덤으로 가져오는 걸로 바꾸었고, 화면사이즈만 조정해주기로!
+    //console.log(window.screen.width, window.screen.height);
+    const screenWidth = window.screen.width;
+    const screenHeight = window.screen.height;
+    console.log(screenWidth, screenHeight);
+    //${screenWidth}x${screenHeight}
+    body.style.backgroundImage = `url(https://source.unsplash.com/1600x900/?nature,landscape)`;
+    
 }
 init();
